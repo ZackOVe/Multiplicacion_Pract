63aded66a69a3f36e070255c246cd42a4043aca5
MexiLander <a.23308051281220@cbtis128.edu.mx>
2024-09-24 19:53:56 -0600

    Add files via upload

---
+++ sisis.py
@@ -0,0 +1,10 @@
+numero1= input("Introduzca el primer numero:")
+num1=int(numero1)
+numero2= input("Introduzca el segundo numero:")
+num2=int(numero2)
+numero3= input("Introduzca el tercer numero: ")
+num3=int(numero3)
+
+multiplicacion = num1 * num2 * num3
+
+print ("El resultado es: ", multiplicacion)
